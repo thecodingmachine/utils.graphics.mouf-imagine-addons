c9f9417ef5e9798e72309dfb79c15aa142f18bd7
ThibBal <tbalmette@gmail.com>
2016-03-01 16:34:35 +0100

    Add feature : dont generate watermark if GET parameter w is equals to zero

--- src/Mouf/Utils/Graphics/ImagineAddons/SmartWatermarkFilter.php
+++ src/Mouf/Utils/Graphics/ImagineAddons/SmartWatermarkFilter.php
@@ -8,6 +8,17 @@ use Imagine\Image\BoxInterface;
 use Imagine\Image\ImageInterface;
 use Imagine\Image\Point;
 
+/**
+ * This filter is used to add a watermark upon an image.
+ *
+ * By default, the watermark is applied. If you want to disable, for any reason, this watermarking,
+ * just add a w parameter in the URL with the value 0 (didn't found better solution from now).
+ * w = 0 : don't apply the watermark
+ * else : apply the watermark
+ *
+ * Class SmartWatermarkFilter
+ * @package Mouf\Utils\Graphics\ImagineAddons
+ */
 class SmartWatermarkFilter implements FilterInterface{
 
     const CENTER = "center";
@@ -65,48 +76,53 @@ class SmartWatermarkFilter implements FilterInterface{
         $watermarkSize = $watermark->getSize();
         $size = $image->getSize();
 
-        switch ($this->position) {
-            case 'top-left':
-                $x = 0;
-                $y = 0;
-                break;
-            case 'top':
-                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
-                $y = 0;
-                break;
-            case 'top-right':
-                $x = $size->getWidth() - $watermarkSize->getWidth();
-                $y = 0;
-                break;
-            case 'left':
-                $x = 0;
-                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
-                break;
-            case 'center':
-                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
-                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
-                break;
-            case 'right':
-                $x = $size->getWidth() - $watermarkSize->getWidth();
-                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
-                break;
-            case 'bottom-left':
-                $x = 0;
-                $y = $size->getHeight() - $watermarkSize->getHeight();
-                break;
-            case 'bottom':
-                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
-                $y = $size->getHeight() - $watermarkSize->getHeight();
-                break;
-            case 'bottom-right':
-                $x = $size->getWidth() - $watermarkSize->getWidth();
-                $y = $size->getHeight() - $watermarkSize->getHeight();
-                break;
-            default:
-                throw new \InvalidArgumentException("Unexpected position '$this->position'");
-                break;
+        if (isset($_GET["w"]) && $_GET["w"] == 0) {
+            return $image;
+        } else {
+            switch ($this->position) {
+                case 'top-left':
+                    $x = 0;
+                    $y = 0;
+                    break;
+                case 'top':
+                    $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                    $y = 0;
+                    break;
+                case 'top-right':
+                    $x = $size->getWidth() - $watermarkSize->getWidth();
+                    $y = 0;
+                    break;
+                case 'left':
+                    $x = 0;
+                    $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                    break;
+                case 'center':
+                    $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                    $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                    break;
+                case 'right':
+                    $x = $size->getWidth() - $watermarkSize->getWidth();
+                    $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                    break;
+                case 'bottom-left':
+                    $x = 0;
+                    $y = $size->getHeight() - $watermarkSize->getHeight();
+                    break;
+                case 'bottom':
+                    $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                    $y = $size->getHeight() - $watermarkSize->getHeight();
+                    break;
+                case 'bottom-right':
+                    $x = $size->getWidth() - $watermarkSize->getWidth();
+                    $y = $size->getHeight() - $watermarkSize->getHeight();
+                    break;
+                default:
+                    throw new \InvalidArgumentException("Unexpected position '$this->position'");
+                    break;
+            }
+            return $image->paste($watermark, new Point($x, $y));
         }
-        return $image->paste($watermark, new Point($x, $y));
+
     }
 
 }
